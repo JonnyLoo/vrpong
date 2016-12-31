d0defe8941e7024e12e979eea90d5f3bc0a6b0e8
JonnyLoo <jonliu44@gmail.com>
2016-12-30 19:59:38 -0500

    first commit

---
+++ Assets/_Script/BallMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+    private float xSpeed, ySpeed, zSpeed;
+    private Vector3 origPoint;
+
+
+	void Start () {
+        xSpeed = (float) 10;
+        ySpeed = (float) 10;
+        zSpeed = (float) 10;
+
+        origPoint = transform.position;
+	}
+	
+	void Update () {
+        transform.Translate(new Vector3 (xSpeed, ySpeed, zSpeed) * Time.deltaTime);
+
+        if(transform.position.z > 34 || transform.position.z < -30)
+        {
+            transform.position = origPoint;
+        }
+	}
+
+    void OnTriggerEnter(Collider other){
+        string tag = other.gameObject.tag;
+        if (tag.CompareTo("player")  == 0)
+        {
+            zSpeed *= -1;
+        }
+        else if(tag.CompareTo("sides") == 0)
+        {
+            xSpeed *= -1;
+        }
+        else
+        {
+            ySpeed *= -1;
+        }
+    }
+}
---
+++ Assets/_Script/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+	}
+
+	void Update() {
+
+    }
+
+	//Before any physics code
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+		rb.MovePosition(transform.position + movement);
+	}
+
+    void OnCollisonEnter(Collision collision) {
+        if(collision.gameObject.name == "Ceiling" || collision.gameObject.name == "East Wall" || collision.gameObject.name == "West Wall" || collision.gameObject.name == "Floor") {
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
